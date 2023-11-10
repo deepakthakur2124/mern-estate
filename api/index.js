2ad316d7037671394f921aac8177bd33dfea7a65
deepakthakur2124 <deepakthakur2124@gmail.com>
2023-11-10 17:16:15 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.LINK)
+  .then(() => {
+    console.log("Mongo DB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is Running on Port 4000");
+  console.log("Server is Running at Prt 3000");
 });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
         <Routes>
     
     <Route path="/" element={<Home />} />
-    <Route path="/home" element={<Home />} />
+    <Route path="/home " element={<Home />} />
     <Route path="about" element={<About />} />
     <Route path="sign-in" element={<SignIn />} />
     <Route path="sign-up" element={<SignUp />} />
